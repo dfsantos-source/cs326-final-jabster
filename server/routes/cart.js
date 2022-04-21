a091f0d648780e0330ff64dd48b23d08178faf2d
Ali Rabeea <aaa.rabeea1411@gmail.com>
2022-04-20 21:48:48 -0400

    heroku configuration / user profile client side

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get("/:userId/get", (req, res) => {
     const userId = req.params.userId
-    const data = [{ id: faker.random.number({ max: 100 }), userId: userId, name: "butter", amount: "2 tablespoons" }, { id: 2, userId: userId, name: "milk", amount: "1 gallon" }, { id: 3, userId: userId, name: "eggs", amount: "1 dozen" }]
+    const data = [{ id: faker.random.number({ max: 100 }), userId: userId, name: "butter", amount: "2 tablespoons" , img: faker.image.food(640, 480, true),}, { id: 2, userId: userId, name: "milk", amount: "1 gallon", img: faker.image.food(640, 480, true)}, { id: 3, userId: userId, name: "eggs", amount: "1 dozen", img: faker.image.food(640, 480, true) }]
     if (userId !== undefined) {
         res.status(200).json(data);
     }
@@ -18,7 +18,7 @@ router.post("/:userId/add", (req, res) => {
     const body = req.body;
     const userId = req.params.userId;
     if (body !== undefined && userId !== undefined) {
-        res.status(200).json({ messsage: "successfully created cart item", data: { id: 1, userId: userId, name: body.name, amount: body.amount } });
+        res.status(200).json({ messsage: "successfully created cart item", data: { id: 1, userId: userId, name: body.name, amount: body.amount, img: faker.image.food(640, 480, true)} });
     }
     else {
         res.status(404).json({ messsage: "body or userId not found" });
