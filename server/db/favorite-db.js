186767c6980c60171a1abeb20d912ce392c63d25
Ali Rabeea <aaa.rabeea1411@gmail.com>
2022-05-06 00:51:32 -0400

    fixed favorties db queries

--- server/db/favorite-db.js
+++ server/db/favorite-db.js
@@ -2,12 +2,16 @@ import client from "./db.js";
 
 
 export const getFavorites = async (userId) => {
-    const queryText = "Select * from User_Favorites WHERE id = $1"
+    const queryText =
+        `Select User_Favorites.id, Recipe_Posts.name, Recipe_Posts.image, Recipe_Posts.ingredients, Recipe_Posts.description, Recipe_Posts.directions
+    from User_Favorites 
+    INNER JOIN Recipe_Posts ON User_Favorites.postId = Recipe_Posts.id 
+    WHERE User_Favorites.userId = $1`
     const res = await client.query(queryText, [userId])
     return res.rows
 }
 
-export const addFavorites = async (body, userId) => {
+export const addFavorites = async (userId, body) => {
     const { postId } = body
     const queryText = "INSERT INTO User_Favorites (userId, postId) VALUES ($1, $2) RETURNING *"
     const res = await client.query(queryText, [userId, postId])
@@ -15,8 +19,7 @@ export const addFavorites = async (body, userId) => {
 }
 
 export const deleteFavorite = async (postId) => {
-    const queryText = "DELETE FROM User_Favorite WHERE id = $1"
+    const queryText = "DELETE FROM User_Favorites WHERE id = $1"
     const res = await client.query(queryText, [postId])
     return res.rows
 }
-
