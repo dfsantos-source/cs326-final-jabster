9872ff8d1797bc8e42abc3aabf2870e63e74e5e8
Ali Rabeea <aaa.rabeea1411@gmail.com>
2022-05-06 00:50:02 -0400

    fixed create table query

--- server/db/db.js
+++ server/db/db.js
@@ -13,52 +13,52 @@ const client = await pool.connect();
 async function initDB() {
     const queryText = `
         CREATE TABLE if not exists Users(
-            "id" Serial PRIMARY KEY,
-            "username" VARCHAR(30) UNIQUE
-            "email" VARCHAR(255),
-            "name" VARCHAR(255),
-            "password" VARCHAR(255)
+            id Serial PRIMARY KEY,
+            username VARCHAR(30) UNIQUE,
+            email VARCHAR(255),
+            name VARCHAR(255),
+            password VARCHAR(255)
         );
         
         CREATE TABLE if not exists Recipe_Posts(
-            "id" Serial PRIMARY KEY,
-            "name" VARCHAR,
-            "userId" INTEGER REFERENCES Users(id),
-            "cuisine" VARCHAR NOT NULL,
-            "likes" INTEGER NOT NULL,
-            "dislikes" INTEGER NOT NULL,
-            "description" VARCHAR NOT NULL,
-            "directions" VARCHAR NOT NULL,
-            "image" VARCHAR NOT NULL,
-            "Ingredients" VARCHAR NOT NULL,
-            "tag" VARCHAR(50) NOT NULL
+            id Serial PRIMARY KEY,
+            name VARCHAR,
+            userId INTEGER REFERENCES Users(id),
+            cuisine VARCHAR NOT NULL,
+            likes INTEGER NOT NULL,
+            dislikes INTEGER NOT NULL,
+            description VARCHAR NOT NULL,
+            directions VARCHAR NOT NULL,
+            image VARCHAR NOT NULL,
+            ingredients VARCHAR NOT NULL,
+            tag VARCHAR(50) NOT NULL
         );
         
         CREATE TABLE if not exists User_Favorites(
-            "id" Serial PRIMARY KEY,
-            "userId" INTEGER REFERENCES Users(id),
-            "postId" INTEGER REFERENCES Recipe_Posts(id)
+            id Serial PRIMARY KEY,
+            userId INTEGER REFERENCES Users(id),
+            postId INTEGER REFERENCES Recipe_Posts(id)
         );
         
         
         CREATE TABLE if not exists Cart_Item(
-            "id" Serial PRIMARY KEY,
-            "userId" INTEGER REFERENCES Users(id),
-            "name" VARCHAR(255) ,
-            "amount" VARCHAR(255)
+            id Serial PRIMARY KEY,
+            userId INTEGER REFERENCES Users(id),
+            name VARCHAR(255) ,
+            amount VARCHAR(255)
         );
         
         
         CREATE TABLE if not exists User_Likes(
-            "id" Serial PRIMARY KEY,
-            "userId" INTEGER REFERENCES Users(id),
-            "postId" INTEGER REFERENCES Recipe_Posts(id)
+            id Serial PRIMARY KEY,
+            userId INTEGER REFERENCES Users(id),
+            postId INTEGER REFERENCES Recipe_Posts(id)
         );
         
         CREATE TABLE if not exists User_Dislikes(
-            "id" Serial PRIMARY KEY,
-            "userId" INTEGER REFERENCES Users(id),
-            "postId" INTEGER REFERENCES Recipe_Posts(id)
+            id Serial PRIMARY KEY,
+            userId INTEGER REFERENCES Users(id),
+            postId INTEGER REFERENCES Recipe_Posts(id)
         );         
     `;
     client.query(queryText);
