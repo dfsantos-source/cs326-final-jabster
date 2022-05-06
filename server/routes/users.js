86153d6b0cb60a8224a9e8bd5e55c65ead13301d
Ali Rabeea <aaa.rabeea1411@gmail.com>
2022-05-06 00:59:32 -0400

    fixed users endpoints

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,53 +1,47 @@
 import express, { response } from 'express'
 import faker from '@faker-js/faker'
+import * as userQueries from '../db/users-db.js'
 
 const router = express.Router()
 
-router.post("/login", (req, res) => {
-    let body = req.body
-    console.log(req.body)
-    if (body.email === undefined || body.password === undefined) {
-        res.status(404).json("email or password not put entered")
-    }
-    else {
-        res.status(200).json("User has logged in succesfully")
-    }
-})
-
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const body = req.body
-    if (body.name === undefined || body.email === undefined || body.email === undefined) {
-        res.status(404).json("registration failed")
-    }
-    else {
-        res.status(200).json({ message: "user registered successfully", "user": req.body });
-    }
-})
 
-router.get("/get/:userId", (req, res) => {
-    const id = req.params.userId;
-    if (id !== undefined) {
-        const user = {
-            id: id,
-            email: faker.internet.email(),
-            name: faker.name.findName()
-        }
+    try {
+        const user = await userQueries.registerUser(body)
         res.status(200).json(user)
-    }
-    else {
-        res.status(400).json("Could not find user")
+
+    } catch (error) {
+        res.status(400).json(error)
     }
 })
 
-router.put('/update/:userId', (req, res) => {
-    let id = req.params.userId;
+router.get("/get/:userId", async (req, res) => {
+    try {
+        const userId = req.user.id
+        const user = await userQueries.findUserById(userId)
+        res.status(200).json(user);
+
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+router.put('/update/:userId', async (req, res) => {
+    let id = req.user.userId;
     const body = req.body;
 
-    if (id !== undefined && body !== undefined) {
-        res.status(200).json({ message: "user successfully updated", user: { id: id, ...body } })
-    }
-    else {
-        res.status(404).json("Failed to update user")
+    try {
+        if (body.username !== undefined) {
+            const newUser = await userQueries.updateName(id, body)
+            res.status(200).json(newUser)
+        }
+        else if (body.password !== undefined) {
+            const newPass = await userQueries.updatePassword(id, body)
+            res.status(200).json(newPass)
+        }
+    } catch (error) {
+        res.status(400).json(error)
     }
 
 })
