a6e56b42d874fc2f41edcdd3bb12e1c905a0fe66
Nolan LaRochelle <nlarochelle@umass.edu>
2022-05-06 01:11:57 -0400

    minor bug fixes to posts-db.js file

--- server/db/posts-db.js
+++ server/db/posts-db.js
@@ -1,19 +1,19 @@
 import client from "./db.js";
 
 export const createPost = async (userId, body) => {
-    const queryText = "INSERT INTO Recipe_Posts(name, userId, cuisine, likes, dislikes, description, directions, image, Ingredients, tag) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
+    const queryText = "INSERT INTO Recipe_Posts(name, userId, cuisine, likes, dislikes, description, directions, image, ingredients, tag) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *";
     const res = await client.query(queryText, [body.name, userId, body.cuisine, 0, 0, body.description, body.directions, body.image, body.ingredients, body.tag]);
     return res.rows
 }
 
 export const updatePost = async (postId, body) => {
-    const queryText = "UPDATE Recipe_Posts SET name = ($1), description = ($2), Ingredients = ($3), directions = ($4), tag = ($5), cuisine = ($6) WHERE id = ($7)";
+    const queryText = "UPDATE Recipe_Posts SET name = ($1), description = ($2), ingredients = ($3), directions = ($4), tag = ($5), cuisine = ($6) WHERE id = ($7) RETURNING *";
     const res = await client.query(queryText, [body.name, body.description, body.ingredients, body.directions, body.tag, body.cuisine, postId]);
     return res.rows
 }
 
 export const deletePost = async (postId) => {
-    const queryText = "DELETE * FROM Recipe_Posts WHERE id = ($1)";
+    const queryText = "DELETE FROM Recipe_Posts WHERE id = ($1)";
     const res = await client.query(queryText, [postId]);
     return res.rows
 }
@@ -60,13 +60,13 @@ export const getPostbyId = async (postId) => {
 }
 
 export const likePost = async (postId) => {
-    const queryText = "UPDATE likes = likes + 1 WHERE id = ($1)"
+    const queryText = "UPDATE Recipe_Posts SET likes = likes + 1 WHERE id = ($1)"
     const res = await client.query(queryText, [postId]);
     return res.rows
 }
 
 export const dislikePost = async (postId) => {
-    const queryText = "UPDATE dislikes = dislikes + 1 WHERE id = ($1)"
+    const queryText = "UPDATE Recipe_Posts SET dislikes = dislikes + 1 WHERE id = ($1)"
     const res = await client.query(queryText, [postId]);
     return res.rows
 }
