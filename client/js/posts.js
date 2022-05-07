61f9d5a812f5bf0db5ac26d1702f3de88ab9a382
Ali Rabeea <aaa.rabeea1411@gmail.com>
2022-05-07 18:30:46 -0400

    fixed posts

--- client/js/posts.js
+++ client/js/posts.js
@@ -6,7 +6,14 @@ async function likePost(data) {
         },
     })
     const dataRes = await response.json()
-    console.log(dataRes)
+    const stat = await response.status
+    if (stat === 400) {
+        alert("You have already liked this post")
+    }
+    if (stat === 200) {
+        renderPage()
+        alert("You have successfully liked the post")
+    }
 }
 
 async function dislikePost(data) {
@@ -17,17 +24,33 @@ async function dislikePost(data) {
         },
     })
     const dataRes = await response.json()
-    console.log(dataRes)
+    const stat = await response.status
+    if (stat === 400) {
+        alert("You have already disliked this post")
+    }
+    if (stat === 200) {
+        renderPage()
+        alert("You have successfully disliked the post")
+    }
 }
 
 async function favoritePost(data) {
-    const response = await fetch(`/user/favorites/${data.id}/add`, {
+    console.log(data.id)
+    const response = await fetch(`/user/favorites/user/add`, {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
         },
+        body: JSON.stringify({ postId: data.id })
     })
     const dataRes = await response.json()
+    const stat = await response.status
+    if (stat === 400) {
+        alert("You have already favorited this post")
+    }
+    if (stat === 200) {
+        alert("Successfully favorited post")
+    }
     console.log(dataRes)
 }
 
@@ -35,14 +58,14 @@ const addPost = (data) => {
     const temp = document.getElementsByTagName("template")[0];
     const clon = temp.content.cloneNode(true);
     clon.getElementById('post-template-title').innerText = data.name;
-    clon.getElementById('post-template-cuisine').innerText = data.cuisine;
+    clon.getElementById('post-template-cuisine').innerText = data.cuisine === "none" ? "" : data.cuisine;
     clon.getElementById('post-template-description').innerText = data.description;
-    clon.getElementById('post-template-img').src = data.img;
-    clon.getElementById('post-template-author').innerText = 'By: User' + data.user_id;
+    clon.getElementById('post-template-img').src = 'data:image/png;base64,' + data.image;
+    clon.getElementById('post-template-author').innerText = 'By: User#' + data.userid;
     const ingredientsList = clon.getElementById('post-template-ingredients');
     const directionsList = clon.getElementById('post-template-directions');
-    const ingredients = data.ingredients;
-    const directions = data.directions;
+    const ingredients = data.ingredients.split(', ');
+    const directions = data.directions.split(".");
     ingredients.forEach(ingredient => {
         const li = document.createElement('li');
         li.innerText = ingredient;
@@ -64,20 +87,26 @@ const addPost = (data) => {
 
 const posts = document.getElementById("posts-main");
 
-const res = await fetch('/posts/get/all', {
-    method: "GET",
-    headers: {
-        'Content-Type': 'application/json',
-    },
-})
 
-const data = await res.json()
-const status = await res.status
 
-console.log(res)
-
-if (status === 200) {
-    data.forEach(post => {
-        addPost(post)
+async function renderPage() {
+    posts.innerHTML = ""
+    const res = await fetch('/posts/get/all', {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        },
     })
+    const data = await res.json()
+    const status = await res.status
+
+    console.log(res)
+
+    if (status === 200) {
+        data.forEach(post => {
+            addPost(post)
+        })
+    }
 }
+
+renderPage()
