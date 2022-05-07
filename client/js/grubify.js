aa37ba553aab2ea7b534028852a588956a58d7fb
Nolan LaRochelle <nlarochelle@umass.edu>
2022-05-07 18:39:40 -0400

    fixed grubify page integration, fixed registration page, minor html fixes in login, navbar update in index page

--- client/js/grubify.js
+++ client/js/grubify.js
@@ -1,14 +1,14 @@
 const posts = document.getElementById("grubify-post");
 
 const addPost = (data) => {
-    posts.innerHTML= ""
+    posts.innerHTML = ""
     const temp = document.getElementsByTagName("template")[0];
     const clon = temp.content.cloneNode(true);
     clon.getElementById('grub-post-template-title').innerText = data.name;
     clon.getElementById('grub-post-template-cuisine').innerText = data.cuisine;
     clon.getElementById('grub-post-template-description').innerText = data.description;
-    clon.getElementById('grub-post-template-img').src = data.img;
-    clon.getElementById('grub-post-template-author').innerText = 'By: User' + data.user_id;
+    clon.getElementById('grub-post-template-img').src = 'data:image/png;base64,' + data.image;
+    clon.getElementById('grub-post-template-author').innerText = 'By: User#' + data.id;
     const ingredientsList = clon.getElementById('grub-post-template-ingredients');
     const directionsList = clon.getElementById('grub-post-template-directions');
     const ingredients = data.ingredients;
@@ -35,11 +35,15 @@ const addPost = (data) => {
         })
         const newData = await res.json();
         const status = await res.status;
-        if(status === 200){
-            console.log(newData);
+        if (status === 400) {
+            alert("You have already liked this post")
+        }
+        if (status === 200) {
+            renderSamePost(data);
+            alert("You have successfully liked the post")
         }
     })
-    
+
     clon.getElementById("grub-dislike-img").addEventListener('click', async (e) => {
         const res = await fetch(`/posts/${data.id}/dislike`, {
             method: "PUT",
@@ -49,31 +53,59 @@ const addPost = (data) => {
         })
         const newData = await res.json();
         const status = await res.status;
-        if(status === 200){
-            console.log(newData);
+        if (status === 200) {
+            await renderSamePost(data)
+            alert("You have successfully disliked the post")
+        }
+        if (status === 400) {
+            alert("You have already disliked this post")
         }
     })
 
     posts.appendChild(clon);
 }
 
+async function renderSamePost(data) {
+    const res = await fetch(`/posts/get/${data.id}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    const resData = await res.json()
+    const postData = resData[0]
+    postData.ingredients = postData.ingredients.split(', ')
+    postData.directions = postData.directions.split(". ")
+    console.log(postData)
+    addPost(postData)
+}
+
 document.getElementById('grubify-butt').addEventListener('click', async (e) => {
-    const dietary_tag = document.getElementById('diet').value;
-    const cuisine_tag = document.getElementById('cuisine').value;
+    const dietary_tag = document.getElementById('diet-select').value;
+    const cuisine_tag = document.getElementById('cuisine-select').value;
+    console.log(cuisine_tag)
 
     document.getElementById('title').innerText = "Not in the Mood?";
     document.getElementById('grubify-butt').innerHTML = "Grubify Again!";
 
-    const res = await fetch('/posts/get/random', {
+    const body = {
+        tag: dietary_tag,
+        cuisine: cuisine_tag
+    }
+    const res = await fetch(`/posts/get/random/${body.tag}&${body.cuisine}`, {
         method: "GET",
         headers: {
             'Content-Type': 'application/json',
         },
     })
 
-    const data = await res.json();
+    const data = await res.json().catch((err) => alert("No results found, try again"));
+
+    data.ingredients = data.ingredients.split(', ');
+    data.directions = data.directions.split('.')
     const status = await res.status;
-    if(status === 200){
+    console.log(data)
+    if (status === 200) {
         addPost(data);
     }
 })
--- client/js/register.js
+++ client/js/register.js
@@ -1,14 +1,15 @@
 
 document.getElementById('register_button').addEventListener('click', async (e) => {
     const name = document.getElementById('name').value;
+    const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const register = {
         name: name,
+        username: username,
         email: email,
         password: password
     }
-
     const res = await fetch('/user/register', {
         method: "POST",
         headers: {
@@ -22,5 +23,8 @@ document.getElementById('register_button').addEventListener('click', async (e) =
 
     if (status === 200) {
         window.location.href = "/client/html/grubify.html";
+    }
+    else {
+        window.location.href = "/client/html/register.html"
     }
 })
